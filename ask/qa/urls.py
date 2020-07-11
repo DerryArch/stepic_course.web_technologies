a20e0ebc59e1a4bfead7384385dfe30c62f1944c
DerryArch <67269300+DerryArch@users.noreply.github.com>
2020-07-11 22:11:40 +0700

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [                                                                 
     path('login/', admin.site.urls),
-	path('', views.test, name='qa_list'),
-	path('login/', views.test, name='login'),
+    path('', views.test, name='qa_list'),
+    path('login/', views.test, name='login'),
 	path('signup/', views.test, name='signup'),
 	path('question/<int:id>/', views.test, name='question'),
 	path('ask/', views.test, name='ask'),
